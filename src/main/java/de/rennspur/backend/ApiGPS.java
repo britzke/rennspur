eadc519e21ae5c3f67b898d44d64e894db4f9a3d
e4_maurer <e4_maurer@localhost>
2016-11-23 11:08:29 +0100

    Altered the getJSON method.

--- src/main/java/de/rennspur/backend/ApiGPS.java
+++ src/main/java/de/rennspur/backend/ApiGPS.java
@@ -25,15 +25,24 @@ package de.rennspur.backend;
 
 public class ApiGPS {
 
+	/**
+	 * Receives the Post from the GPS-component.
+	 * @return c String with content of JSON.
+	 */
 	@GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("{id}")
-    public String getCustomer(@PathParam("id") int id) {
-        string c = "" + id;
-        return c;
-    }
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public String getJSON(@PathParam("id") int id) {
+		string c = "" + id;
+		return c;
+	}
 
-	public void parseJson() {
+	/**
+	 * Parses the received JSON File into single arguments which are then uploaded
+	 * to the Database.
+	 * @param json The JSON to parse.
+	 */
+	public void parseJson(MediaType.APPLICATION_JSON json) {
 		JSONParser parser = new JSONParser();
 
 		try {
@@ -41,18 +50,6 @@ public class ApiGPS {
 			Object obj = parser.parse(new FileReader("c:\\test.json"));
 
 			JSONObject jsonObject = (JSONObject) obj;
-
-			/*
-			 * String name = (String) jsonObject.get("name");
-			 * System.out.println(name);
-			 * 
-			 * long age = (Long) jsonObject.get("age"); System.out.println(age);
-			 * 
-			 * // loop array JSONArray msg = (JSONArray)
-			 * jsonObject.get("messages"); Iterator<String> iterator =
-			 * msg.iterator(); while (iterator.hasNext()) {
-			 * System.out.println(iterator.next()); }
-			 */
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
