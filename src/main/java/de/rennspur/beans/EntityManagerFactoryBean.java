b7eb0cc2cb3559d7a887657e6ffc8b9c77021a12
burghard.britzke <bubi@charmides.in-berlin.de>
2016-11-30 07:26:05 +0100

    EntityManagerFactory bean for database access

---
+++ src/main/java/de/rennspur/beans/EntityManagerFactoryBean.java
@@ -0,0 +1,63 @@
+/*
+ *  This file is part of Renspur.
+ *  
+ *  Copyright (C) 2016  burghard.britzke, bubi@charmides.in-berlin.de
+ *  
+ *  Rennspur is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Rennspur is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rennspur.beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * The EntityManagerFactoryBean provides an application scoped
+ * EntityManagerFactory. Because the EntityManagerFactory itself is not
+ * Serializable, it can only injected in <b>transient</b> properties, if the
+ * bean into which is incected is {@code @SessionScoped}.
+ * 
+ * @see Serializable
+ * @author burghard.britzke
+ */
+@ApplicationScoped
+public class EntityManagerFactoryBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Produces
+	private EntityManagerFactory entityManagerFactory;
+
+	/**
+	 * Creates a new EntityManagerFactoryBean.
+	 */
+	public EntityManagerFactoryBean() {
+		entityManagerFactory = Persistence
+				.createEntityManagerFactory("rennspur-db");
+	}
+
+	/**
+	 * Gets the entityManagerFactory.
+	 * 
+	 * @return the entityManager
+	 */
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+}
---
+++ src/main/java/de/rennspur/beans/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package are managed (CDI) beans.
+ * 
+ * @author burghard.britzke
+ */
+package de.rennspur.beans;
