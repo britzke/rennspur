767c2fd7c54d0523863d0681dbfb6393973d2c46
Max Lee <itsaplay@gmail.com>
2016-12-07 14:52:06 +0100

    Points are drawable now. Insert first code to draw lines.

--- src/main/webapp/playground/frontend/osmMap.js
+++ src/main/webapp/playground/frontend/osmMap.js
@@ -1,5 +1,5 @@
 /*
- *  This file is part of Renspur.
+ *  This file is part of Rennspur.
  *  
  *  Copyright (C) 2016  maximilian lietzmann, justin beutler, kevin ivancea
  *  
@@ -23,56 +23,123 @@ var map;
  * Position and zoomlevel of the map
  */
 
-var lon = 13.34148;
-var lat = 52.49603;
-var zoom = 18;
-				
-var language = 'de';
+var lon = 13.3425;
+var lat = 52.49605;
 
+var lon1 = 13.3435;
+var lat1 = 52.49615;
+
+/*var pointsObj = {
+	'type' : 'FeatureCollection',
+	'features' : [
+			{
+				'type' : 'Feature',
+				'geometry' : {
+					'type' : 'Polygon',
+					'coordinates' : [ [ [ -5e6, 6e6 ], [ -5e6, 8e6 ],
+							[ -3e6, 8e6 ], [ -3e6, 6e6 ], [ -5e6, 6e6 ] ] ]
+				}
+			},
+			{
+				'type' : 'Feature',
+				'geometry' : {
+					'type' : 'Polygon',
+					'coordinates' : [ [ [ -2e6, 6e6 ], [ -2e6, 8e6 ],
+							[ 0, 8e6 ], [ 0, 6e6 ], [ -2e6, 6e6 ] ] ]
+				}
+			},
+			{
+				'type' : 'Feature',
+				'geometry' : {
+					'type' : 'Polygon',
+					'coordinates' : [ [ [ 1e6, 6e6 ], [ 1e6, 8e6 ],
+							[ 3e6, 8e6 ], [ 3e6, 6e6 ], [ 1e6, 6e6 ] ] ]
+				}
+			},
+			{
+				'type' : 'Feature',
+				'geometry' : {
+					'type' : 'Polygon',
+					'coordinates' : [ [ [ -2e6, -1e6 ], [ -1e6, 1e6 ],
+							[ 0, -1e6 ], [ -2e6, -1e6 ] ] ]
+				}
+			} ]
+}; */
+
+var zoom = 18;
 /**
  * Draws the map.
  * 
  * @returns nothing
  */
 function drawmap() {
-	
-	/* var earthquakeStyle = new ol.style.Style({
-        image: new ol.style.Icon({
-          anchor: [0.5, 0.5],
-          size: [52, 52],
-          offset: [52, 0],
-          opacity: 1,
-          scale: 0.25,
-          src: 'images.png'
-        })
-      }); */
-	
-	var center = ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
-	
-    var view = new ol.View({
-    	center: center,
-        zoom: zoom
-      });
-   
-    var tile = new ol.layer.Tile({
-    	             source: new ol.source.OSM()
-    	             });
-    
-    map = new ol.Map({
-    	view: view,
-    	layers: [ tile ],
-    	target: "mapdiv"
-	    
-	});   
-	
-}
 
-/**
- * Map event listener moved
- * 
- * @param event
- * @returns
- */
-function mapEventMove(event) {
+	var iconFeature = new ol.Feature({
+		geometry : new ol.geom.Point(ol.proj.transform([ lon, lat ],
+				'EPSG:4326', 'EPSG:3857')),
+	});
+
+	var iconStyle = new ol.style.Style({
+		image : new ol.style.Icon(
+		({
+			src : 'marker.png'
+		}))
+	});
+
+	iconFeature.setStyle(iconStyle);
+	// -------------------------------------------------------------------
+	var iconFeature1 = new ol.Feature({
+		geometry : new ol.geom.Point(ol.proj.transform([ lon1, lat1 ],
+				'EPSG:4326', 'EPSG:3857'))
+	});
+
+	var iconStyle1 = new ol.style.Style({
+		image : new ol.style.Icon(
+		({
+			src : 'marker.png'
+		}))
+	});
+
+	iconFeature1.setStyle(iconStyle1);
+	// -------------------------------------------------------------------
+
+	// -------------------------------------------------------------------
+
+	var vectorSource = new ol.source.Vector({
+		features : [ iconFeature, iconFeature1 ]
+	});
+
+	var vectorLayer = new ol.layer.Vector({
+		source : vectorSource
+	});
+
+	var center = ol.proj.transform([ lon, lat ], 'EPSG:4326', 'EPSG:3857');
+
+	var view = new ol.View({
+		center : center,
+		zoom : zoom
+	});
+
+	var tile = new ol.layer.Tile({
+		source : new ol.source.OSM()
+	});
+
+	map = new ol.Map({
+		view : view,
+		renderer : 'canvas',
+		layers : [ tile, vectorLayer ],
+		target : "mapdiv"
+
+	});
+
+	/**
+	 * Map event listener moved
+	 * 
+	 * @param event
+	 * @returns
+	 */
+	function mapEventMove(event) {
+
+	}
 
 }
