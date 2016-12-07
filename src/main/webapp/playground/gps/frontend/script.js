d9e7e939ee80f9cf2079294564d1e51b7b44833f
seka94 <kazim.sevgi@hotmail.de>
2016-12-07 14:50:27 +0100

    Update #3

--- src/main/webapp/playground/gps/frontend/script.js
+++ src/main/webapp/playground/gps/frontend/script.js
@@ -1,4 +1,7 @@
 
+var width = 0;
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -7,27 +10,47 @@ function myFunction() {
 
 function ersteFunction() {
      
-    	window.onclick = function(event) {
-    	  if (event.target.matches('.dropbtn')) {
-    		  
-    		  system.out.println("haha")
-    		  
-    	  }
-    }
+	document.getElementById("select-button").innerHTML = "Erste Boje"
+	document.getElementById("myDropdown").classList.toggle("show");	
 }
 
 function zweiteFunction() {
+	document.getElementById("select-button").innerHTML = "Zweite Boje"
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 function dritteFunction() {
+	document.getElementById("select-button").innerHTML = "Dritte Boje"
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 function vierteFunction() {
+	document.getElementById("select-button").innerHTML = "Vierte Boje"
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function sendeFunction(){
+	
+//	document.getElementById("progress-bar").width = 25%
+	
+	konniFunction();
+	bojeweniger();
+	
+}
+
+function konniFunction(){
+	//alert("konnifunction")
+}
+
+function bojeweniger(){
+	if(document.getElementById("select-button").innerHTML != "Wähle eine Boje"){
+		alert("Passt.. andere Boje");
+		width += 25;
+		alert(width);
+	}
+	else 
+		alert("Wählen Sie bitte eine Boje");
+}
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
