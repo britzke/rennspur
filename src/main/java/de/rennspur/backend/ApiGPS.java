6b8a889ab2d16a493d6dbdf8d78f744bb7a5bacf
Ruben Maurer <maurerruben97@gmail.com>
2016-12-07 13:59:33 +0100

    #6 Updated the ApiGPS class to handly JSON Strings.

--- src/main/java/de/rennspur/backend/ApiGPS.java
+++ src/main/java/de/rennspur/backend/ApiGPS.java
@@ -23,12 +23,15 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -38,9 +41,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.primefaces.json.JSONArray;
+import org.primefaces.json.JSONException;
 import org.primefaces.json.JSONObject;
 
 import de.rennspur.model.Club;
+import de.rennspur.model.Clubs;
+import de.rennspur.model.Event;
+import de.rennspur.model.Race;
 import de.rennspur.model.TeamPosition;
 
 /**
@@ -85,72 +92,89 @@ public class ApiGPS {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getGPSDataInJSON(String jsonString) {
 
-		/**
-		 * Test Output.
-		 */
-		System.out.println(jsonString);
-		System.out.println(emf);
+		
+		String key, name,longitude,latitude;
+				
+		Clubs clubs = new Clubs();  
+		
+		JSONArray lineItems;
+		ArrayList<TeamPosition> positionList;
+		JSONObject json;
 
 		/**
 		 * Initializing Variables.
 		 */
-		ArrayList<TeamPosition> positionList = new ArrayList<TeamPosition>();
+		positionList = new ArrayList<TeamPosition>();
+		json = new JSONObject(jsonString);
 
 		/**
-		 * Converting the Input String into a JSONObject
+		 * Extracting the key value from the JSON Object.
 		 */
-		JSONObject json = new JSONObject(jsonString);
+		try {
+			key = json.getString("key");
+			System.out.println("Boat key : " + key);
+			
+			if(key == "" || key == null)
+				key = "0";
+
+		} catch (JSONException e) {
+			System.out.println("Failed extracting values for key & name!");
+			e.printStackTrace();
+			key = "0";
+		}
 
 		/**
-		 * Extracting the key and name value off the boat.
+		 * Extracting the position-array from the JSONObject and handling
+		 * possible exceptions.
 		 */
-		String key = (String) json.get("key");
-		String name = (String) json.get("name");
-
-		/**
-		 * Test Output.
-		 */
-		System.out.println(key + " : " + name);
-
-		/**
-		 * Extracting the position-array from the JSONObject.
-		 */
-		JSONArray lineItems = json.getJSONArray("lineItems");
+		try {
+			lineItems = json.getJSONArray("status");
+		} catch (JSONException e) {
+			System.out.println("Failed extracting lineItems array!");
+			e.printStackTrace();
+			lineItems = null;
+		}
 
 		/**
 		 * Itterating through the extracted arry from the JSONObject, to get
 		 * every position.
 		 */
-		for (Object o : lineItems) {
+		if (lineItems != null) {
+			for (Object o : lineItems) {
 
-			JSONObject jsonLineItem = (JSONObject) o;
+				System.out.println(o.toString());
+				JSONObject jsonPostitionObject = new JSONObject(o.toString());
 
-			String longitude = jsonLineItem.getString("key");
-			String latitude = jsonLineItem.getString("value");
-			String time = jsonLineItem.getString("value");
+				/**
+				 * Creating a new TeampositionObject and filling it with the
+				 * extracted Data from the JSONObject.
+				 * 
+				 * TeamPosition newPosition = new TeamPosition();
+				 * newPosition.setId(Integer.parseInt(key));
+				 * newPosition.setLatitude(Double.parseDouble(latitude));
+				 * newPosition.setLongitude(Double.parseDouble(longitude));
+				 * newPosition.setTime(convertStringToTimestamp(time));
+				 */
+				
+				TeamPosition newPosition = new TeamPosition();
+				
+				newPosition.setId(Integer.parseInt(key));
+				newPosition.setLatitude(jsonPostitionObject.getDouble("lat"));
+				newPosition.setLongitude(jsonPostitionObject.getDouble("long"));
+				
+				/**
+				 * Adding the new now filled TeamPosition object into the
+				 * Arraylist.
+				 * 
+				 * 
+				 */
+				
+				positionList.add(newPosition);
 
-			/**
-			 * Creating a new TeampositionObject and filling it with the
-			 * extracted Data from the JSONObject.
-			 */
-			TeamPosition newPosition = new TeamPosition();
-			newPosition.setId(Integer.parseInt(key));
-			newPosition.setLatitude(Double.parseDouble(latitude));
-			newPosition.setLongitude(Double.parseDouble(longitude));
-			newPosition.setTime(convertStringToTimestamp(time));
-
-			/**
-			 * Adding the new now filled TeamPosition object into the Arraylist.
-			 */
-			positionList.add(newPosition);
-
-			/**
-			 * Test Output.
-			 */
-			System.out.println(longitude + " : " + latitude + " : " + time);
+			}
 		}
 
-		return "ok";
+		return " ok";
 	}
 
 	/**
