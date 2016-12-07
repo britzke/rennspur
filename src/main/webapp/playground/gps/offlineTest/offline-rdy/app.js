9d5e857fa16580b51f8135e2794337fcf586a146
Tim Prangel <tim.prangel@gmail.com>
2016-12-07 08:55:53 +0100

    #8 forgot 2 files...

---
+++ src/main/webapp/playground/gps/offlineTest/offline-rdy/app.js
@@ -0,0 +1,41 @@
+/*
+ *  This file is part of Rennspur.
+ *  
+ *  Copyright (C) 2016  burghard.britzke, bubi@charmides.in-berlin.de
+ *  
+ *  Rennspur is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Rennspur is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * Register a service worker
+ */ 
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+    
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
+  }).catch(function(error) {
+    
+    console.log('Registration failed with ' + error);
+  });
+}
+
---
+++ src/main/webapp/playground/gps/offlineTest/offline-rdy/sw.js
@@ -0,0 +1,48 @@
+/*
+ *  This file is part of Rennspur.
+ *  
+ *  Copyright (C) 2016  burghard.britzke, bubi@charmides.in-berlin.de
+ *  
+ *  Rennspur is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Rennspur is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Define which files the service worker caches.
+ */
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'CHANGE ME',
+        'CHANGE ME'
+    })
+  );
+});
+/*
+ * Check if there are changes in the cached files. If yes update the cache
+ */
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/test1.jpg'); // CHANGE ME
+  }));
+});
