0f1d41acb5a259e49f5f7ba8ccd146cddbbd8903
e4_ivancea <eldiegos@hotmail.de>
2016-12-14 14:15:22 +0100

    #9  update

--- src/main/webapp/playground/frontend/gpsHandler.js
+++ src/main/webapp/playground/frontend/gpsHandler.js
@@ -2,43 +2,23 @@ $(document).ready(function () {
   $("body").prepend(function () {
     var showData = $('#show');
 
-    $.getJSON('testData.json', function (data) {
-      console.log(data);
-      
-      var items = [];
-      $.each( data, function(key,val) {
-        items.push( "<li id='" + key + "'>" + val + "</li>" );
-      });
-     
-      $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-      }).appendTo( "body" );
- 
-    });
-
-    showData.text('test');
+    var json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "testData.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })(); 
     
-    var jqxhr = $.getJSON( "testData.json", function() {
-    	  console.log( "success" );
-    	})
-    	  .done(function() {
-    	    console.log( "second success" );
-    	  })
-    	  .fail(function() {
-    	    console.log( "error" );
-    	  })
-    	  .always(function() {
-    	    console.log( "complete" );
-    	  });
-    	 
-    	// Perform other work here ...
-    	 
-    	// Set another completion function for the request above
-    	jqxhr.complete(function() {
-    	  console.log( "second complete" );
-    	});
-
+    alert(json.Boot1position.lat);
+    
+   
 
     
   });
