7cfd8ba38f1ddeaf217971a6b3c8eb7e34f94c6c
britzke <bubi@charmides.in-berlin.de>
2017-04-04 10:01:24 +0200

    #40 Test for the HandleEvent Method

---
+++ src/test/java/de/rennspur/test/init/db/UnmarschallerValidationEventHandlerTest.java
@@ -0,0 +1,158 @@
+/**
+ * 
+ */
+package de.rennspur.test.init.db;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventLocator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Node;
+
+import de.rennspur.init.db.UnmarschallerValidationEventHandler;
+
+/**
+ *
+ *
+ * @author burghard.britzke
+ */
+public class UnmarschallerValidationEventHandlerTest {
+
+	private final class ValidationEventImpl implements ValidationEvent {
+		private final class ValidationEventLocatorImpl implements ValidationEventLocator {
+			private boolean getURLCalled;
+			private boolean getColNumberCalled;
+
+			@Override
+			public URL getURL() {
+				getURLCalled = true;
+				return null;
+			}
+
+			@Override
+			public int getOffset() {
+				return 0;
+			}
+
+			@Override
+			public Object getObject() {
+				return null;
+			}
+
+			@Override
+			public Node getNode() {
+				return null;
+			}
+
+			@Override
+			public int getLineNumber() {
+				boolean getLineNumberCalled = true;
+				return 0;
+			}
+
+			@Override
+			public int getColumnNumber() {
+				getColNumberCalled = true;
+				return 0;
+			}
+
+			/**
+			 * @return the getURLCalled
+			 */
+			public boolean isGetURLCalled() {
+				return getURLCalled;
+			}
+
+			/**
+			 * @return the getColNumberCalled
+			 */
+			public boolean isGetColNumberCalled() {
+				return getColNumberCalled;
+			}
+		}
+
+		private boolean getLocatorCalled;
+		private boolean getMessageCalled;
+
+		/**
+		 * Returns ValidationEvent.ERROR - every time for test purposes.
+		 * 
+		 * @see javax.xml.bind.ValidationEvent#getSeverity()
+		 */
+		@Override
+		public int getSeverity() {
+
+			return ValidationEvent.ERROR;
+		}
+
+		@Override
+		public String getMessage() {
+			getMessageCalled = true;
+			return null;
+		}
+
+		@Override
+		public ValidationEventLocator getLocator() {
+			getLocatorCalled = true;
+			if (locator == null) {
+				locator = new ValidationEventLocatorImpl();
+			}
+			return locator;
+		}
+
+		@Override
+		public Throwable getLinkedException() {
+			return null;
+		}
+
+		/**
+		 * @return the getLocatorCalled
+		 */
+		public boolean isGetLocatorCalled() {
+			return getLocatorCalled;
+		}
+
+		/**
+		 * @return the getMessageCalled
+		 */
+		public boolean isGetMessageCalled() {
+			return getMessageCalled;
+		}
+	}
+
+	private UnmarschallerValidationEventHandler proband;
+	private ValidationEventLocator locator;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		proband = new UnmarschallerValidationEventHandler();
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.rennspur.init.db.UnmarschallerValidationEventHandler#handleEvent(javax.xml.bind.ValidationEvent)}.
+	 */
+	@Test
+	public void testHandleEvent() {
+		ValidationEventImpl ve;
+		ve = new ValidationEventImpl();
+		// 2. send test message
+		proband.handleEvent(ve);
+		// 3. proof result
+		assertTrue("getLocator() must be called on ValidationEvent", ve.isGetLocatorCalled());
+		assertTrue("getMessage() must be called on ValidationEvent", ve.isGetMessageCalled());
+		assertTrue("getURL() must be called on the validationEvent's validationEventLocator",
+				((de.rennspur.test.init.db.UnmarschallerValidationEventHandlerTest.ValidationEventImpl.ValidationEventLocatorImpl) ve
+						.getLocator()).isGetURLCalled());
+	}
+
+}
